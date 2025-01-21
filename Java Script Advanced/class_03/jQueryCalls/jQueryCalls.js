fb6eef6503f2474f6d62b560b6c3749f16c034b3
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-01-22 02:39:08 +0800

    jquerycalls part2

--- Java Script Advanced/class_03/jQueryCalls/jQueryCalls.js
+++ Java Script Advanced/class_03/jQueryCalls/jQueryCalls.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
     $.ajax({
       url: "https://raw.githubusercontent.com/qa-codecademy/mkwd13-04-ajs/refs/heads/main/shared_data/students.json",
       success: function (response) {
-        debugger
+        debugger;
         console.log(response);
         //response JSON _> js object
         let parsedObject = JSON.parse(response);
         console.log(parsedObject);
-        for(let student of parsedObject.students){
-            $("#students").append(`<li>${student}</li>`)
+        for (let student of parsedObject.students) {
+          $("#students").append(`<li>${student}</li>`);
         }
-        
       },
       error: function (response) {
         console.log("The request failed");
@@ -19,4 +18,23 @@ $(document).ready(function () {
       },
     });
   });
+
+
+$("#getAstrosBtn").click(function () {
+  $.ajax({
+    url: "http://api.open-notify.org/astros.json",
+    success: function (response) {
+        debugger
+      console.log(response);
+      for(let astro of response.people){
+        $("#students").append(`<li>${astro.name}</li>`)
+      }
+    },
+    error: function (error) {
+      console.log("An error occured");
+      console.log(error);
+    },
+  });
+});
+
 });
