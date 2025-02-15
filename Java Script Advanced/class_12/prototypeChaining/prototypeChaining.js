90c44cbad48be2ba9817d8c7252e5fdf71ce4e25
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-02-16 05:37:36 +0800

    prototype chaining

---
+++ Java Script Advanced/class_12/prototypeChaining/prototypeChaining.js
@@ -0,0 +1,33 @@
+function Vehicle(id, name, seriesNum, price) {
+    debugger
+    this.id = id;
+    this.name = name;
+    this.seriesNum = seriesNum;
+    this.price = price;
+    this.printVehicle = function()
+    {
+      console.log(
+        `Info: ${this.id}  ${this.inamed}  ${this.price}  ${this.seriesNum}`
+      );
+    }
+  }
+
+  let vehicle = new Vehicle(4, "test", '5555', 10000);
+
+  function WheeledVehicle(id, name, seriesNum, price, numberOfWheels){
+    //we are saying that every instance of wheeled vehicle has vehicle as prototype
+   //and we let it set the values of the properties for each instance in the Vehicle cons functi
+    debugger
+    Object.setPrototypeOf(this, new Vehicle(id, name, seriesNum, price));
+    this.numberOfWheels = numberOfWheels;
+    this.drive = function(){
+        console.log("You are driving a wheeled vehicle.");
+    }
+  }
+
+  let wheeledVehicle = new WheeledVehicle(1, "BMW", "123123", 20000, 4);
+  console.log(wheeledVehicle);
+
+  let wheeledVehicle2 = new WheeledVehicle();
+  console.log(wheeledVehicle2);
+  
